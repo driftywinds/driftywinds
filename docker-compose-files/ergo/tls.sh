bfb12649c905f7365272f2f5637978b7ec38ac38
drifty <amoghtiwari@duck.com>
2025-01-16 12:37:33 +0530

    Create tls.sh

---
+++ docker-compose-files/ergo/tls.sh
@@ -0,0 +1,11 @@
+cd /path/to/ergo
+docker compose down
+sudo rm /path/to/ergo/data/fullchain.pem
+sudo rm /path/to/ergo/data/privkey.pem
+sudo cp /etc/letsencrypt/live/irc.example.com/fullchain.pem /path/to/ergo/data
+sudo cp /etc/letsencrypt/live/irc.example.com/privkey.pem /path/to/ergo/data
+cd /path/to/ergo
+docker compose up -d
+# curl -m 10 --retry 5 https://hc-ping.com/
+# the above is for healthchecks.io monitoring
+# set this in cron like this 24 1 17 2,5,8,11 * sudo bash /path/to/tls.sh
