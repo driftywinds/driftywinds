a492ce2fdab6c3d87eaf6414ea20b6a289918206
drifty <amoghtiwari@duck.com>
2025-01-16 12:38:36 +0530

    Update tls.sh

--- docker-compose-files/ergo/tls.sh
+++ docker-compose-files/ergo/tls.sh
@@ -8,4 +8,5 @@ cd /path/to/ergo
 docker compose up -d
 # curl -m 10 --retry 5 https://hc-ping.com/
 # the above is for healthchecks.io monitoring
+# this script assumes you've configured the TLS certificates to auto renew every 3 months using cert-bot or some other utility
 # set this in cron like this 24 1 17 2,5,8,11 * sudo bash /path/to/tls.sh
